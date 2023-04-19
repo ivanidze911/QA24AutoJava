b1a31c7e6e33e42509254664e111e860f4d7c220
ivanidze911 <ivahka090@gmail.com>
2023-04-19 11:11:49 +0300

    Merge remote-tracking branch 'origin/master'

--- src/QA24AutoJava/Homework/Hw2/MainСalculator.java
+++ src/QA24AutoJava/Homework/Hw2/MainСalculator.java
@@ -1,5 +1,7 @@
 package QA24AutoJava.Homework.Hw1;
 
+import QA24AutoJava.Homework.Hw2.Calculatop;
+
 public class MainСalculator {
     public static void main(String[] args) {
         Calculatop calc = new Calculatop();
